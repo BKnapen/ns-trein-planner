4b90d31c35489c5fe2e88834ed6481d821912abc
BKnapen <bram@rsstv4u.com>
2023-08-23 14:36:04 +0200

    -

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+    require_once "vendor/autoload.php";
+    
+    $tripplanner = new TripPlanner();
+    $tripplanner->set_fromStation("Eindhoven");
+    $tripplanner->set_toStation("Utrecht");
+    $tripplanner->set_departDateTime("2023-08-23T20:00:00+0200");
+    
+    $url = "https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/trips?";
+    $url .= "fromStation=".$tripplanner->get_fromStation()."&";
+    $url .= "toStation=".$tripplanner->get_toStation()."&";
+    $url .= "originWalk=false&";
+    $url .= "originBike=false&"; 
+    $url .= "originCar=false&";
+    $url .= "destinationWalk=false&";
+    $url .= "destinationBike=false&";
+    $url .= "destinationCar=false&";
+    $url .= "dateTime=".$tripplanner->set_departDateTime()."&";
+    $url .= "shorterChange=false&";
+    $url .= "travelAssistance=false&";
+    $url .= "searchForAccessibleTrip=false&";
+    $url .= "localTrainsOnly=false&";
+    $url .= "excludeHighSpeedTrains=false&";
+    $url .= "excludeTrainsWithReservationRequired=false&";
+    $url .= "product=GEEN&";
+    $url .= "discount=NO_DISCOUNT&";
+    $url .= "travelClass=2&";
+    $url .= "passing=false&";
+    $url .= "travelRequestType=DEFAULT";
+
+    $curl = curl_init($url);
+    
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    
+    # Request headers
+    $headers = array(
+        'Cache-Control: no-cache',
+        'Ocp-Apim-Subscription-Key: 81c6df58fd86454b80577991531182c3',);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    
+    $resp = curl_exec($curl);
+    curl_close($curl);
+    var_dump($resp);
+?>
--- src/TripPlanner.php
+++ src/TripPlanner.php
@@ -2,6 +2,32 @@
     namespace Webkompanen\Nstreinplanner;
 
     class TripPlanner{
-        
+        public $fromStation;
+        public $toStation;
+        public $departDateTime;
+
+        // Tripplanner depart information
+
+        function set_fromStation($fromStation) {
+            $this->fromStation = $fromStation;
+        }
+        function get_fromStation() {
+            return $this->fromStation;
+        }
+
+        function set_departDateTime($departDateTime) {
+            $this->departDateTime = $departDateTime;
+        }
+        function get_departDateTime() {
+            return $this->departDateTime;
+        }
+
+        // Tripplanner arrival information
+        function set_toStation($toStation) {
+            $this->toStation = $toStation;
+        }
+        function get_toStation() {
+            return $this->toStation;
+        }
     }
 ?>
