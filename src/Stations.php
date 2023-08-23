cb2fbca773023d470d0f45169a988ebe92d6cf32
BKnapen <bram@rsstv4u.com>
2023-08-23 15:05:40 +0200

    -

--- src/Stations.php
+++ src/Stations.php
@@ -3,5 +3,12 @@
 
     class Stations{
         public $queryRequest;
+
+        function set_queryRequest($queryRequest) {
+            $this->queryRequest = $queryRequest;
+        }
+        function get_queryRequest() {
+            return $this->queryRequest;
+        }
     }
 ?>
